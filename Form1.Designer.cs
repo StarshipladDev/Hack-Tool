2e766901da49919ebfac3ba82b91fdc82e1b9f20
StarshipladDev <StarshipladDev@gmail.com>
2019-04-14 23:09:02 +1200

    Url Fuzzing testing
    The new files change the program from a simple GUI to a similarly crappy GUI that actually does things. It takes a URL input, then tests that input for GET SQL and XSS attack vulnerabilities, then displays which vulnerabilities exist if any.
    :::::::::::CHANGES:::::::::::
    Added a text-box (Editable / non-scrollable). Program takes values in the text-box and performs several HTTP requests.
    Label now outputs which variables are vulnerable and which kinds of attack they are vulnerable to.
    When button is pressed, the value in textbox(must be request with php GET values) is cleansed down to its individual variables, then each will be given an illegal XSS or SQLI  addition, then a new HTTP request will be issued with fuzzed data, and any valid responses are displayed in the programs label as vulnerable to the tested addition.
    Added new log entry to development log.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.b = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.textBox = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // b
@@ -42,6 +43,18 @@
             this.b.Text = "Begin hack";
             this.b.UseVisualStyleBackColor = false;
             this.b.Click += new System.EventHandler(this.button1_Click);
+            //
+            //Text box
+            //
+            
+            this.textBox.Location = new System.Drawing.Point(150,0);
+            this.textBox.Name = "b";
+            this.textBox.Size = new System.Drawing.Size(250,250);
+            this.textBox.TabIndex = 1;
+            this.textBox.Text = "Type url with Get method here";
+
+
+            //
             // 
             // label1
             // 
@@ -49,7 +62,7 @@
             this.label1.Location = new System.Drawing.Point(332, 134);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(46, 17);
-            this.label1.TabIndex = 1;
+            this.label1.TabIndex = 2;
             this.label1.Text = "label1";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
@@ -61,6 +74,7 @@
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.b);
+            this.Controls.Add(this.textBox);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -72,6 +86,7 @@
 
         private System.Windows.Forms.Button b;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.IO;
 
 namespace Bruteforce1
 {
@@ -21,10 +23,55 @@ namespace Bruteforce1
         {
 
         }
+        /// <summary>
+        /// The method that runs when the button is pressed in the application.
+        /// It takes a hard coded HTTP url with GET method parameters, cleanses that URL down to the passed
+        /// values, and dispalys each parameter passed into the label
+
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void button1_Click(object sender, System.EventArgs e)
         {
-            label1.Text = "changed";
-            label1.Update();
+            //"http://www.starshiplad.com/sendit.php?name=Test+Data&info=This+si+important+information"
+            string url = textBox.Text;
+            String output = "";
+            int index = url.IndexOf('?');
+            int i = 0;
+            string[] urlParameters = url.Remove(0, index + 1).Split('&');
+            string[] phonyrequests = new String[urlParameters.Length];
+            StreamReader reader = null;
+                
+            foreach(string p in urlParameters)
+            {
+                //Run to test for XSS ability
+                phonyrequests[i] = url.Replace(p,p+"<xss>");
+                output += "Attempted to send: "+ phonyrequests[i] + "\n";
+                HttpWebRequest webRequest = (HttpWebRequest)HttpWebRequest.Create(phonyrequests[i]);
+                webRequest.Method = "GET";
+                string response = "";
+                reader = new StreamReader(webRequest.GetResponse().GetResponseStream());
+                response = reader.ReadToEnd();
+                if (response.Contains("<xss>"))
+                {
+                    output += "XSS vunrability in param " + p + "\n";
+                }
+                //Repeat for SQL injection
+                phonyrequests[i] = url.Replace(p, p + "bad'info");
+                output += "Attempted to send: " + phonyrequests[i] + "\n";
+                webRequest = (HttpWebRequest)HttpWebRequest.Create(phonyrequests[i]);
+                webRequest.Method = "GET";
+                response = "";
+
+                reader = new StreamReader(webRequest.GetResponse().GetResponseStream());
+                response = reader.ReadToEnd();
+                if (response.Contains("bad'info"))
+                {
+                    output += "SQL injection vunrability in param " + p +"\n";
+                }
+                i++;
+            }
+            label1.Text = output;
         }
     }
 }
